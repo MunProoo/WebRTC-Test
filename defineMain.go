b69b85da447d2c941778b90c6d80162795b503b7
MunProoo <mjo5178@gmail.com>
2024-07-30 17:18:32 +0900

    Revert "init"
    This reverts commit 9c15d02d29e66285419bc9ee2afcdefe146eba67.

--- defineMain.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "github.com/pion/webrtc/v4"
-
-type PeerConnectionManager struct {
-	PeerConnection *webrtc.PeerConnection
-	OutputTracks   map[string]*webrtc.TrackLocalStaticRTP
-}
